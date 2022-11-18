b8d3c449fc2e05a17143e3cf7794b2e3a49d4c19
xxxsjan <626653354@qq.com>
2022-11-19 02:37:39 +0800

    加点好玩的

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,122 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue';
+import gsap from 'gsap';
+
+function onClick() {}
+onMounted(() => {
+  const { clientWidth, clientHeight } = document.documentElement;
+  function randomX() {
+    return Math.floor(Math.random() * (clientWidth - 100 - 100)) + 100;
+  }
+  function randomY() {
+    return Math.floor(Math.random() * (clientHeight - 100 - 100)) + 100;
+  }
+  function start() {
+    setTimeout(() => {
+      gsap.to('#as', { x: randomX(), y: randomY(), duration: 1 });
+      gsap.to('#ngm', { x: randomX(), y: randomY(), duration: 1 });
+      start();
+    }, 888);
+  }
+  start();
+
+  const ngmDom = document.querySelector('#ngm');
+  const audio = document.querySelector('#audio');
+  const music_switch = document.querySelector('.music_switch');
+  audio.muted = true;
+  ngmDom.addEventListener('mouseenter', (e) => {
+    audio.play();
+  });
+  music_switch.onclick = function () {
+    if (audio.muted) {
+      audio.muted = false;
+      music_switch.classList.add('active');
+    } else {
+      audio.muted = true;
+      console.log(audio.muted);
+      music_switch.classList.remove('active');
+    }
+  };
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="as">阿松</div>
+  <div id="ngm" @click="onClick">你干嘛</div>
+  <div class="music_switch">
+    <span class="s1"></span>
+    <span class="s2"></span>
+    <span class="s3"></span>
+    <span class="s4"></span>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <audio id="audio" src="./a.mp3"></audio>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+#as {
+  font-size: 50px;
+  display: inline-block;
+  color: #fff;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+#ngm {
+  font-size: 50px;
+  display: inline-block;
+  color: #fff;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.music_switch {
+  position: fixed;
+  width: 40px;
+  height: 30px;
+  top: 48px;
+  right: 62px;
+  cursor: pointer;
+  z-index: 10;
+}
+.music_switch span {
+  position: absolute;
+  width: 4px;
+  bottom: 0;
+  background: #fff;
+}
+.music_switch .s1 {
+  height: 11px;
+  right: 0px;
+}
+.music_switch .s2 {
+  height: 21px;
+  right: 10px;
+}
+.music_switch .s3 {
+  height: 28px;
+  right: 20px;
+}
+.music_switch .s4 {
+  height: 13px;
+  right: 30px;
+}
+.music_switch.active .s1 {
+  animation: wave 0.66s linear infinite;
+  -webkit-animation: wave 0.66s linear infinite;
+  -ms-animation: wave 0.66s linear infinite;
+}
+.music_switch.active .s2 {
+  animation: wave 0.8s linear infinite;
+}
+.music_switch.active .s3 {
+  animation: wave 0.7s linear infinite;
+}
+.music_switch.active .s4 {
+  animation: wave 0.5s linear infinite;
+}
+@keyframes wave {
+  0% {
+    height: 10px;
+  }
+  50% {
+    height: 28px;
+  }
+  100% {
+    height: 10px;
+  }
 }
 </style>
